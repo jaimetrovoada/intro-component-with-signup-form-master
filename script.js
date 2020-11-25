ae01e995c46a8d9e6bb1e35b6834cfad20c2698f
JaimeTrovoada <jaimetrovoada@gmail.com>
2020-11-25 15:24:13 +0800

    script prototype

--- script.js
+++ script.js
@@ -1,10 +1,40 @@
-const Msg = document.querySelectorAll(".errorMsg");
-console.log(Msg);
+/* TODO: when the submit button is clicked it should verify if the field are empty, 
+if empty it should add an inner text to the msg div  */
 
-const submit = document.querySelector("button").onclick;
-function test() {
-  const errorMsg = Msg.innerText("This Field Cannot be Empty");
-  if (document.querySelectorAll("input") == "") {
-    return errorMsg;
+const myForm = document.querySelector("#myForm");
+const firstName = document.querySelector("#firstName");
+const lastName = document.querySelector("#lastName");
+const email = document.querySelector("#email");
+const password = document.querySelector("#pass");
+
+const firstNameMsg = document.querySelector("#firstNameMsg");
+const lastNameMsg = document.querySelector("#lastNameMsg");
+const emailMsg = document.querySelector("#emailMsg");
+const passMsg = document.querySelector("#passMsg");
+
+myForm.addEventListener("submit", onSubmit);
+
+function onSubmit(e) {
+  e.preventDefault();
+
+  if (firstName.value === "") {
+    firstNameMsg.classList.add("errorMsg");
+    firstNameMsg.innerText = "First Name cannot be empty";
+    setTimeout(() => firstNameMsg.remove(), 1500);
+  }
+  if (lastName.value === "") {
+    lastNameMsg.classList.add("errorMsg");
+    lastNameMsg.innerText = "Last Name cannot be empty";
+    setTimeout(() => lastNameMsg.remove(), 1500);
+  }
+  if (email.value === "") {
+    emailMsg.classList.add("errorMsg");
+    emailMsg.innerText = "email cannot be empty";
+    setTimeout(() => emailMsg.remove(), 1500);
+  }
+  if (password.value === "") {
+    passMsg.classList.add("errorMsg");
+    passMsg.innerText = "Password cannot be empty";
+    setTimeout(() => passMsg.remove(), 1500);
   }
 }
